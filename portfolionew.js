da8f91cda157ddb0d9bd66292c66bac2972c1b4f
Vignesh24R <vignesh240102@gmail.com>
2022-02-03 12:26:04 +0530

    initial commit

---
+++ portfolionew.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+    // ================================ scrolling ====================
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+  });
+  
+  
+  let numberPercent = document.querySelectorAll('.countbar')
+          let getPercent = Array.from(numberPercent)
+  
+          getPercent.map((items) => {
+              let startCount = 0
+              let progressBar = () => {
+                  startCount++
+                  items.innerHTML = `<h3>${startCount}%</h3>`
+                  items.style.width = `${startCount}%`
+                  if (startCount == items.dataset.percentnumber) {
+                      clearInterval(stop)
+                  }
+              }
+              let stop = setInterval(() => {
+                  progressBar()
+              }, 25)
+          })
+  
