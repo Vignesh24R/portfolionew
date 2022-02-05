363a5975d607970099c2cd68ab35e239d9a6fe70
Vignesh24R <vignesh240102@gmail.com>
2022-02-05 13:59:07 +0530

    responsive changes

--- portfolionew.js
+++ portfolionew.js
@@ -55,4 +55,16 @@ $(document).ready(function(){
                   progressBar()
               }, 25)
           })
+
+// ========================active link====================
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
   
+  /*Remove menu mobile*/
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
